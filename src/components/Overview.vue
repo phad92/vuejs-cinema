df255408026f4b7204c8ddb7666814ee01c8350e
Fadlu Haruna <mailfadluharuna@gmail.com>
2019-01-11 12:49:32 +0000

    fixed bugs

---
+++ src/components/Overview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="overview">
+    <day-select v-bind:selected="day"></day-select>
+    <div id="main">
+      <movie-list v-bind:day="day" v-bind:genre="genre" v-bind:time="time" v-bind:movies="movies"></movie-list>
+      <movie-filter></movie-filter>
+    </div>
+  </div>
+</template>
+<script>
+import MovieList from "./MovieList.vue";
+import MovieFilter from "./MovieFilter.vue";
+import DaySelect from './DaySelect.vue';
+export default {
+  props: ["genre", "time", "movies", "day"],
+  components: {
+    MovieList,
+    MovieFilter,
+    DaySelect
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,1 +1,67 @@
-console.log("hello world");
+import Vue from "vue";
+import "./style.scss";
+
+// import Overview from './components/Overview.vue';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+import moment from "moment-timezone";
+moment.tz.setDefault("UTC");
+Object.defineProperty(Vue.prototype, '$moment',{ get(){return this.$root.moment}});
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import routes from './util/routes';
+const router = new VueRouter({ routes });
+// import checkFilter from './util/bus';
+import setDay from './util/bus';
+
+const bus = new Vue();
+Object.defineProperty(Vue.prototype,'$bus',{get() {return this.$root.bus}});
+
+new Vue({
+  el: "#app",
+  data: {
+    genre: [],
+    time: [],
+    movies: [],
+    moment,
+    day: moment(),
+    bus
+  },
+  methods:{
+    checkFilter(category, title, checked) {
+      // console.log("check");
+      if (checked) {
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (index > -1) {
+          this[category].splice(index, 1);
+        }
+      }
+    }
+  },
+  created(){
+    this.$http.get('/api').then(response => {
+      console.log(response.data);
+      this.movies = response.data;
+    });
+    var self = this;
+    this.$bus.$on('check-filter', this.checkFilter);
+    // this.$bus.$on('set-day',setDay.bind(this) )
+    this.$bus.$on('set-day',(day) => {this.day = day} )
+  },
+  router
+});
+
+Vue.directive('tooltip',{
+  bind(el,bindings){
+    let span = document.createElement('SPAN');
+    let text = document.createTextNode('Seats available: 200');
+    span.appendChild(text);
+    el.appendChild(span);
+  }
+})
